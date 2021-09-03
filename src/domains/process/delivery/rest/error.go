f3c8a7e9019c51884ac739b82ba28fb548e9c75d
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-09-04 02:19:21 +0700

    change error response argsn📝

--- src/domains/process/delivery/rest/error.go
+++ src/domains/process/delivery/rest/error.go
@@ -1,16 +1,15 @@
 package rest
 
 import (
-	"net/http"
-
 	"github.com/hrz8/sc-masterlist-service/src/utils"
 )
 
 type (
 	ProcessErrorInterface interface {
-		ProcessErrorCreate(*utils.CustomContext, string) error
-		ProcessErrorGetAll(*utils.CustomContext, string) error
-		ProcessErrorGet(*utils.CustomContext, string) error
+		ProcessErrorCreate(*utils.CustomContext, *string, *uint16) error
+		ProcessErrorGetAll(*utils.CustomContext, *string, *uint16) error
+		ProcessErrorGet(*utils.CustomContext, *string, *uint16) error
+		ProcessErrorDelete(*utils.CustomContext, *string, *uint16) error
 	}
 
 	processErrorImpl struct {
@@ -18,16 +17,20 @@ type (
 	}
 )
 
-func (i *processErrorImpl) ProcessErrorCreate(ctx *utils.CustomContext, message string) error {
-	return ctx.ErrorResponse(nil, message, http.StatusBadRequest, i.prefix+"-001", nil)
+func (i *processErrorImpl) ProcessErrorCreate(ctx *utils.CustomContext, message *string, status *uint16) error {
+	return ctx.ErrorResponse(nil, *message, *status, i.prefix+"-001", nil)
 }
 
-func (i *processErrorImpl) ProcessErrorGetAll(ctx *utils.CustomContext, message string) error {
-	return ctx.ErrorResponse(nil, message, http.StatusBadRequest, i.prefix+"-002", nil)
+func (i *processErrorImpl) ProcessErrorGetAll(ctx *utils.CustomContext, message *string, status *uint16) error {
+	return ctx.ErrorResponse(nil, *message, *status, i.prefix+"-002", nil)
 }
 
-func (i *processErrorImpl) ProcessErrorGet(ctx *utils.CustomContext, message string) error {
-	return ctx.ErrorResponse(nil, message, http.StatusBadRequest, i.prefix+"-003", nil)
+func (i *processErrorImpl) ProcessErrorGet(ctx *utils.CustomContext, message *string, status *uint16) error {
+	return ctx.ErrorResponse(nil, *message, *status, i.prefix+"-003", nil)
+}
+
+func (i *processErrorImpl) ProcessErrorDelete(ctx *utils.CustomContext, message *string, status *uint16) error {
+	return ctx.ErrorResponse(nil, *message, *status, i.prefix+"-004", nil)
 }
 
 func NewProcessError() ProcessErrorInterface {
--- src/utils/context.go
+++ src/utils/context.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"github.com/hrz8/sc-masterlist-service/src/helpers"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
 )
@@ -13,21 +14,21 @@ type (
 	}
 )
 
-func (c *CustomContext) SuccessResponse(data interface{}, message string, status int, meta interface{}) error {
-	return c.JSON(status, &SuccessResponse{
+func (c *CustomContext) SuccessResponse(data interface{}, message string, status uint16, meta interface{}) error {
+	return c.JSON(int(status), &SuccessResponse{
 		Data:    data,
 		Message: message,
-		Status:  status,
+		Status:  int(status),
 		Meta:    meta,
 	})
 }
 
-func (c *CustomContext) ErrorResponse(data interface{}, message string, status int, errorCode string, meta interface{}) error {
-	return c.JSON(status, &ErrorResponse{
-		Data:      data,
+func (c *CustomContext) ErrorResponse(data interface{}, message string, status uint16, errorCode string, meta interface{}) error {
+	return c.JSON(int(status), &ErrorResponse{
+		Data:      helpers.NilToEmptyMap(&data),
 		Message:   message,
-		Status:    status,
+		Status:    int(status),
 		ErrorCode: errorCode,
-		Meta:      meta,
+		Meta:      helpers.NilToEmptyMap(&meta),
 	})
 }
