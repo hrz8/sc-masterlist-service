57630a1cdd3d4b3cfd319c1ab564e57f9b66110d
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-09-10 22:02:36 +0700

    enhance error for project

--- src/domains/project/delivery/rest/error.go
+++ src/domains/project/delivery/rest/error.go
@@ -1,36 +1,122 @@
 package rest
 
 import (
+	"errors"
+	"net/http"
+
+	"github.com/hrz8/sc-masterlist-service/src/helpers"
 	"github.com/hrz8/sc-masterlist-service/src/utils"
 )
 
 type (
 	ProjectErrorInterface interface {
-		ProjectErrorCreate(ctx *utils.CustomContext, message *string, status *uint16) error
-		ProjectErrorGetAll(ctx *utils.CustomContext, message *string, status *uint16) error
-		ProjectErrorGet(ctx *utils.CustomContext, message *string, status *uint16) error
-		ProjectErrorDelete(ctx *utils.CustomContext, message *string, status *uint16) error
+		Throw(ctx *utils.CustomContext, domainErr error, err error) error
 	}
 
 	projectErrorImpl struct {
 		prefix string
 	}
+
+	projectErrorMap struct {
+		status int
+		err    error
+	}
 )
 
-func (i *projectErrorImpl) ProjectErrorCreate(ctx *utils.CustomContext, message *string, status *uint16) error {
-	return ctx.ErrorResponse(nil, *message, *status, i.prefix+"-001", nil)
-}
+var (
+	ProjectErrorCreate = projectErrorMap{
+		status: 400,
+		err:    errors.New("failed to store project"),
+	}
+	ProjectErrorGetAll = projectErrorMap{
+		status: 400,
+		err:    errors.New("failed to list project"),
+	}
+	ProjectErrorGetById = projectErrorMap{
+		status: 400,
+		err:    errors.New("failed to get project"),
+	}
+	ProjectErrorDeleteById = projectErrorMap{
+		status: 400,
+		err:    errors.New("failed to remove project"),
+	}
+	ProjectErrorUpdateById = projectErrorMap{
+		status: 400,
+		err:    errors.New("failed to update project"),
+	}
+)
 
-func (i *projectErrorImpl) ProjectErrorGetAll(ctx *utils.CustomContext, message *string, status *uint16) error {
-	return ctx.ErrorResponse(nil, *message, *status, i.prefix+"-002", nil)
-}
-
-func (i *projectErrorImpl) ProjectErrorGet(ctx *utils.CustomContext, message *string, status *uint16) error {
-	return ctx.ErrorResponse(nil, *message, *status, i.prefix+"-003", nil)
-}
-
-func (i *projectErrorImpl) ProjectErrorDelete(ctx *utils.CustomContext, message *string, status *uint16) error {
-	return ctx.ErrorResponse(nil, *message, *status, i.prefix+"-004", nil)
+func (i *projectErrorImpl) Throw(ctx *utils.CustomContext, domainErr error, err error) error {
+	if errors.Is(domainErr, ProjectErrorCreate.err) {
+		status := uint16(ProjectErrorCreate.status)
+		return ctx.ErrorResponse(
+			map[string]interface{}{
+				"reason": err.Error(),
+			},
+			domainErr.Error(),
+			status,
+			i.prefix+"-001",
+			nil,
+		)
+	}
+	if errors.Is(domainErr, ProjectErrorGetAll.err) {
+		status := uint16(ProjectErrorGetAll.status)
+		return ctx.ErrorResponse(
+			map[string]interface{}{
+				"reason": err.Error(),
+			},
+			domainErr.Error(),
+			status,
+			i.prefix+"-002",
+			nil,
+		)
+	}
+	if errors.Is(domainErr, ProjectErrorGetById.err) {
+		errStatus := uint16(ProjectErrorGetById.status)
+		status := helpers.ParseStatusResponse(err, errStatus)
+		return ctx.ErrorResponse(
+			map[string]interface{}{
+				"reason": err.Error(),
+			},
+			domainErr.Error(),
+			status,
+			i.prefix+"-003",
+			nil,
+		)
+	}
+	if errors.Is(domainErr, ProjectErrorDeleteById.err) {
+		errStatus := uint16(ProjectErrorDeleteById.status)
+		status := helpers.ParseStatusResponse(err, errStatus)
+		return ctx.ErrorResponse(
+			map[string]interface{}{
+				"reason": err.Error(),
+			},
+			domainErr.Error(),
+			status,
+			i.prefix+"-004",
+			nil,
+		)
+	}
+	if errors.Is(domainErr, ProjectErrorUpdateById.err) {
+		errStatus := uint16(ProjectErrorUpdateById.status)
+		status := helpers.ParseStatusResponse(err, errStatus)
+		return ctx.ErrorResponse(
+			map[string]interface{}{
+				"reason": err.Error(),
+			},
+			domainErr.Error(),
+			status,
+			i.prefix+"-005",
+			nil,
+		)
+	}
+	return ctx.ErrorResponse(
+		nil,
+		"Internal Server Error",
+		http.StatusInternalServerError,
+		i.prefix+"-REST-500",
+		nil,
+	)
 }
 
 func NewProjectError() ProjectErrorInterface {
