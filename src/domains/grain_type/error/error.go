d92e9e6869d4cc4560fcda030287d73f7f0dea4b
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-09-16 01:03:53 +0700

    add domain error ✂️

---
+++ src/domains/grain_type/error/error.go
@@ -0,0 +1,33 @@
+package error
+
+import "errors"
+
+type (
+	errorMap struct {
+		Status int
+		Err    error
+	}
+)
+
+var (
+	Create = errorMap{
+		Status: 400,
+		Err:    errors.New("failed to store grain type"),
+	}
+	GetAll = errorMap{
+		Status: 400,
+		Err:    errors.New("failed to list grain type"),
+	}
+	GetById = errorMap{
+		Status: 400,
+		Err:    errors.New("failed to get grain type"),
+	}
+	DeleteById = errorMap{
+		Status: 400,
+		Err:    errors.New("failed to remove grain type"),
+	}
+	UpdateById = errorMap{
+		Status: 400,
+		Err:    errors.New("failed to update grain type"),
+	}
+)
