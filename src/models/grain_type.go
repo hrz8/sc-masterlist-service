73791033a6fbd277dcc31faf93e55ec3865d2bc7
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-09-16 00:56:40 +0700

    add grain type models

--- src/models/grain_type.go
+++ src/models/grain_type.go
@@ -7,27 +7,27 @@ import (
 	"gorm.io/gorm"
 )
 
-// Project represents Project object for DB
+// GrainType represents GrainType object for DB
 type (
-	Project struct {
+	GrainType struct {
 		ID          uuid.UUID      `gorm:"column:id;primaryKey" json:"id"`
-		Name        string         `gorm:"column:name;index:idx_name;unique;not null" json:"name" validate:"required"`
+		Code        string         `gorm:"column:name;index:idx_name;unique;not null" json:"name" validate:"required"`
 		Description string         `gorm:"column:description" json:"description"`
 		CreatedAt   time.Time      `gorm:"column:created_at" json:"createdAt"`
 		UpdatedAt   time.Time      `gorm:"column:updated_at" json:"updatedAt"`
 		DeletedAt   gorm.DeletedAt `gorm:"column:deleted_at;index" json:"-"`
 	}
 
-	// ProjectPayloadCreate represents payload to create project
-	ProjectPayloadCreate struct {
-		Name        string `json:"name" validate:"required,max=50"`
+	// GrainTypePayloadCreate represents payload to create grain type
+	GrainTypePayloadCreate struct {
+		Code        string `json:"name" validate:"required,max=50"`
 		Description string `json:"description" validate:"max=140"`
 	}
 
-	// ProjectPayloadGetAll represents payload to fetch all projects
-	ProjectPayloadGetAll struct {
+	// GrainTypePayloadGetAll represents payload to fetch all grain types
+	GrainTypePayloadGetAll struct {
 		// column
-		Name        FilteringQueryParams `query:"name"`
+		Code        FilteringQueryParams `query:"name"`
 		Description FilteringQueryParams `query:"description"`
 		// date props
 		CreatedAt FilteringQueryParams `query:"createdAt"`
@@ -38,20 +38,20 @@ type (
 		Deleted    DeleteQueryParams `query:"_deleted"`
 	}
 
-	// ProjectPayloadGet represents payload to get project by identifier
-	ProjectPayloadGet struct {
+	// GrainTypePayloadGet represents payload to get grain type by identifier
+	GrainTypePayloadGet struct {
 		ID uuid.UUID `param:"id" validate:"required"`
 	}
 
-	// ProjectPayloadUpdateById represents payload to update project by identifier
-	ProjectPayloadUpdateById struct {
+	// GrainTypePayloadUpdateById represents payload to update grain type by identifier
+	GrainTypePayloadUpdateById struct {
 		ID          uuid.UUID `json:"-" param:"id" validate:"required"`
-		Name        string    `json:"name" validate:"max=50"`
+		Code        string    `json:"name" validate:"max=50"`
 		Description string    `json:"description" validate:"max=140"`
 	}
 
-	// ProjectPayloadDeleteById represents payload to delete project by identifier
-	ProjectPayloadDeleteById struct {
+	// GrainTypePayloadDeleteById represents payload to delete grain type by identifier
+	GrainTypePayloadDeleteById struct {
 		ID uuid.UUID `param:"id" validate:"required"`
 	}
 )
