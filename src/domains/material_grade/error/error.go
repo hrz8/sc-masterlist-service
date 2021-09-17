147f5821590c4989beac6818936bf17713223613
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-09-18 01:30:07 +0700

    add material grade error 🔎🧮

---
+++ src/domains/material_grade/error/error.go
@@ -0,0 +1,33 @@
+package error
+
+import "errors"
+
+type (
+	errorMap struct {
+		Status int
+		Err    error
+	}
+)
+
+var (
+	Create = errorMap{
+		Status: 400,
+		Err:    errors.New("failed to store material grade"),
+	}
+	GetAll = errorMap{
+		Status: 400,
+		Err:    errors.New("failed to list material grade"),
+	}
+	GetById = errorMap{
+		Status: 404,
+		Err:    errors.New("failed to get material grade"),
+	}
+	DeleteById = errorMap{
+		Status: 400,
+		Err:    errors.New("failed to remove material grade"),
+	}
+	UpdateById = errorMap{
+		Status: 400,
+		Err:    errors.New("failed to update material grade"),
+	}
+)
