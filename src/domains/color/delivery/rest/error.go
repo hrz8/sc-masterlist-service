e763cd62b584e45a20dfddb79052f2728ca8bb9b
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-09-18 22:46:24 +0700

    add error color

---
+++ src/domains/color/delivery/rest/error.go
@@ -0,0 +1,99 @@
+package rest
+
+import (
+	"errors"
+	"net/http"
+
+	ColorError "github.com/hrz8/sc-masterlist-service/src/domains/color/error"
+	"github.com/hrz8/sc-masterlist-service/src/helpers"
+	"github.com/hrz8/sc-masterlist-service/src/utils"
+)
+
+type (
+	RestErrorInterface interface {
+		Throw(ctx *utils.CustomContext, domainErr error, dataErr error) error
+	}
+
+	restErrorImpl struct {
+		prefix string
+	}
+)
+
+func (i *restErrorImpl) Throw(ctx *utils.CustomContext, domainErr error, dataErr error) error {
+	if errors.Is(domainErr, ColorError.Create.Err) {
+		status := uint16(ColorError.Create.Status)
+		return ctx.ErrorResponse(
+			map[string]interface{}{
+				"reason": dataErr.Error(),
+			},
+			domainErr.Error(),
+			status,
+			i.prefix+"-001",
+			nil,
+		)
+	}
+	if errors.Is(domainErr, ColorError.GetAll.Err) {
+		status := uint16(ColorError.GetAll.Status)
+		return ctx.ErrorResponse(
+			map[string]interface{}{
+				"reason": dataErr.Error(),
+			},
+			domainErr.Error(),
+			status,
+			i.prefix+"-002",
+			nil,
+		)
+	}
+	if errors.Is(domainErr, ColorError.GetById.Err) {
+		errStatus := uint16(ColorError.GetById.Status)
+		status := helpers.ParseStatusResponse(dataErr, errStatus)
+		return ctx.ErrorResponse(
+			map[string]interface{}{
+				"reason": dataErr.Error(),
+			},
+			domainErr.Error(),
+			status,
+			i.prefix+"-003",
+			nil,
+		)
+	}
+	if errors.Is(domainErr, ColorError.DeleteById.Err) {
+		errStatus := uint16(ColorError.DeleteById.Status)
+		status := helpers.ParseStatusResponse(dataErr, errStatus)
+		return ctx.ErrorResponse(
+			map[string]interface{}{
+				"reason": dataErr.Error(),
+			},
+			domainErr.Error(),
+			status,
+			i.prefix+"-004",
+			nil,
+		)
+	}
+	if errors.Is(domainErr, ColorError.UpdateById.Err) {
+		errStatus := uint16(ColorError.UpdateById.Status)
+		status := helpers.ParseStatusResponse(dataErr, errStatus)
+		return ctx.ErrorResponse(
+			map[string]interface{}{
+				"reason": dataErr.Error(),
+			},
+			domainErr.Error(),
+			status,
+			i.prefix+"-005",
+			nil,
+		)
+	}
+	return ctx.ErrorResponse(
+		nil,
+		"Internal Server Error",
+		http.StatusInternalServerError,
+		i.prefix+"-REST-500",
+		nil,
+	)
+}
+
+func NewColorError() RestErrorInterface {
+	return &restErrorImpl{
+		prefix: "SCM-COLOR",
+	}
+}
